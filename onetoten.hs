047c172a31fb89dcba591e2d72b23f2cd917d1aa
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-06-18 16:27:23 -0500

    [Finished] 1st list of problems

--- onetoten.hs
+++ onetoten.hs
@@ -64,3 +64,11 @@ pack [x] = [[x]]
 pack (x:xs) = if x == (head xs)
               then (x:(head $ pack xs)):(tail $ pack xs)
               else [x]:(pack xs)
+
+--10  Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+--     Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of
+--     the element E
+-- encode "aaaabccaadeeee" => [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+encode [] = []
+encode (x:xs) = (length $ x : takeWhile (==x) xs, x)
+                : encode (dropWhile (==x) xs)
