4a76d9b59455e25d76bf0952939b60c433ecf6bf
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-07-18 16:31:36 -0500

    [Solved] 7th problem

--- Project Euler/Problem1.hs
+++ Project Euler/Problem1.hs
@@ -14,3 +14,29 @@ fibonacciAux n a2 a1 s = let sumF = a2 + a1
                                 fibonacciAux (n - 1) a1 (a2 + a1) s
                            else s
 
+primeFactors n = factor n $ [2] ++ [3,5..]
+  where
+    factor n (p:ps)
+        | p*p > n        = [n]
+        | n `mod` p == 0 = p : factor (n `div` p) (p:ps)
+        | otherwise      =     factor n ps
+
+problem_3 = last (primeFactors 600851475143)
+
+problem_4 =
+  maximum [x | y<-[100..999], z<-[y..999], let x=y*z, let s=show x, s==reverse s]
+
+problem_5 = foldr1 lcm [1..20]
+
+problem_6 = (sum [1..100])^2 - sum (map (^2) [1..100])
+
+problem_7 = primeP 2 0 
+
+primeP :: Int -> Int -> Int
+primeP n 10001 =  n - 1
+primeP n c = if null [ x | x <- [2..n - 1], n `mod`x  == 0]
+                then primeP (n + 1) (c + 1) 
+                else primeP (n + 1) c
+
+
+
